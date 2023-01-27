2e467dbcec6fc3e14b59ab1404d70964c9d166bb
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-01-27 12:50:55 +0200

    function

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,30 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+//-----------Генератор випадкових чисел-------------
+function randomInt() {
+  const min = 100;
+  const max = 200;
+  let rand = Math.floor(min + Math.random() * (max + 1 - min));
+  console.log(rand);
+}
+randomInt();
 
+// --------------------------------------------------
+
+let min = 1000;
+let max = 2000;
+
+function numberRand() {
+  let rand = Math.floor(min + Math.random() * (max + 1 - min));
+  console.log(rand);
+}
+numberRand();
+
+// --------------------------------------------------------------------
+
+
+function could() {
+  let c = 9;
+  c++;
+  console.log(c);
+}
+
+could();
