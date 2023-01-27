bd65e25852ef908397b0c11eb7eb58acb9841997
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-01-27 14:12:31 +0200

    boolean

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,40 @@
-//-----------Генератор випадкових чисел-------------
-function randomInt() {
-  const min = 100;
-  const max = 200;
-  let rand = Math.floor(min + Math.random() * (max + 1 - min));
-  console.log(rand);
-}
-randomInt();
+// //-----------Генератор випадкових чисел-------------
+// function randomInt() {
+//   const min = 100;
+//   const max = 200;
+//   let rand = Math.floor(min + Math.random() * (max + 1 - min));
+//   console.log(rand);
+// }
+// randomInt();
 
-// --------------------------------------------------
+// // --------------------------------------------------
 
-let min = 1000;
-let max = 2000;
+// let min = 1000;
+// let max = 2000;
 
-function numberRand() {
-  let rand = Math.floor(min + Math.random() * (max + 1 - min));
-  console.log(rand);
-}
-numberRand();
+// function numberRand() {
+//   let rand = Math.floor(min + Math.random() * (max + 1 - min));
+//   console.log(rand);
+// }
+// numberRand();
 
 // --------------------------------------------------------------------
 
+// ----------цикл--------------------------------------------------
+// let count = 0;
+// while(count < 10){
+//   console.log(count);
+//   if(count==4) {
+//     break;
+//   }
+//   count++
+// }
 
-function could() {
-  let c = 9;
-  c++;
-  console.log(c);
-}
+// ----------------bool--------------------------------------------------------
 
-could();
+var bool = true;
+var bool2 = false;
+
+console.log(bool);
+console.log(bool2);
+console.log(true && true);
