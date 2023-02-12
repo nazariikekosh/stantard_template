df62ee3b410babba1bfee861ac45bd088fc4c64a
nazariikekosh <118050634+nazariikekosh@users.noreply.github.com>
2023-02-12 14:57:05 +0200

    area

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,20 @@
-let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    dots: true,
-    infinite: true
-  });
-});
+function getDataUser() {
+  const userName = document.getElementById('name').value;
+  const userSurname = document.getElementById('surname').value;
+  const userArea = document.getElementById('area').value;
 
+  const userData = {
+    name: `${userName}`,
+    surname: `${userSurname}`,
+    area: `${userArea}`
+
+}
+return userData
+}
+
+const TARGET_BTN = document.getElementById('sub');
+TARGET_BTN.addEventListener('click', (e) => {
+  e.preventDefault()
+  const DATA = getDataUser();
+  console.log(DATA)
+})
